9ef59638893a9b5b2e5ab085565cef0385c58ad4
ETA <eta@fcbm-290246-Macbook.local>
2021-02-25 17:18:08 -0300

    Refectory server.ts and add tests

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import { app } from '../app';
+
+import createConnection from '../database';
+
+
+describe("Surveys", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new Survey", async () => {
+        const response = await request(app).post("/surveys").send({
+            title: "Title Exemple",
+            description: "Description Exemple"
+        });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id")
+    });
+
+    it("Should be able to get all Surveys", async () => {
+        await request(app).post("/surveys").send({
+            title: "Title Exemple 2",
+            description: "Description Exemple 2"
+        });
+
+        const response = await request(app).get("/surveys");
+
+        expect(response.body.length).toBe(2);
+    })
+});
