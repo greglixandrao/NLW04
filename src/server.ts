65a0cbdd65bdfc884111d65aca036a77f4b3a86e
ETA <eta@fcbm-290246-Macbook.local>
2021-02-24 21:18:36 -0300

    fist commit - add all files

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata'
+import express, { request, response } from 'express';
+import "./database";
+import { router } from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => console.log("Server is runing!"));
+
+
+// app.get("/", (request, response) => {
+//     return response.json({ message: "Hello World - NLW04" });
+// })
+
+
+// // 1 parametro - Rota(Recurso API)
+// // 2 paramentro - request, response
+
+// app.post("/", (request, response) => {
+//     return response.json({ message: "Os dados foram salvos com sucesso" });
+// })
